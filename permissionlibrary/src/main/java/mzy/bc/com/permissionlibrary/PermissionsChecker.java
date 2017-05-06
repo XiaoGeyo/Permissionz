f9a67997ab1c42ffbab89b23d9b1c22f7b12d42e
xiaogeyo <381837094@qq.com>
2017-05-06 23:24:28 +0800

    Initial commit

---
+++ permissionlibrary/src/main/java/mzy/bc/com/permissionlibrary/PermissionsChecker.java
@@ -0,0 +1,40 @@
+package mzy.bc.com.permissionlibrary;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * 检查权限的工具类
+ * <p/>
+ * Created by wangchenlong on 16/1/26.
+ */
+public class PermissionsChecker {
+    private final Context mContext;
+    // 所需的全部权限
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+            Manifest.permission.CAMERA
+    };
+    public PermissionsChecker(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    // 判断权限集合
+    public boolean lacksPermissions(String... permissions) {
+        for (String permission : permissions) {
+            if (lacksPermission(permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 判断是否缺少权限
+    private boolean lacksPermission(String permission) {
+        return ContextCompat.checkSelfPermission(mContext, permission) ==
+                PackageManager.PERMISSION_DENIED;
+    }
+}
